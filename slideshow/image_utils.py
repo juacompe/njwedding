1523f1c2c593111453d9b32411407de9d4146a70
juacompe <juacompe@gmail.com>
2011-09-17 04:44:47 +0700

    - home page has slideshow in it - the images in the slideshow is find from collected_images - all test pass

---
+++ slideshow/image_utils.py
@@ -0,0 +1,7 @@
+def collected_images():
+    images = ['132883093_49660b8f2a_z.jpg',
+              '399107666.jpg',
+              '2957442267_6afce8dd8f_z.jpg',
+              '5975249429_4c076b25c1_z.jpg',
+              '6041022237_287ede031f_z.jpg',]
+    return images
--- slideshow/views.py
+++ slideshow/views.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from slideshow.image_utils import collected_images
 
 def home(request):
     context = RequestContext(request)
     context['IMAGE_URL'] = settings.MEDIA_URL + 'collected_twitter_images/'
     context['THUMBNAIL_URL'] = settings.MEDIA_URL + 'collected_twitter_images/thumbnails/'
+    context['images'] = collected_images()
     return render_to_response('home.html', context_instance=context)
+
