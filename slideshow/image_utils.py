4178f6a10cf6472b205bf5b286422160de92f777
juacompe <juacompe@gmail.com>
2011-09-17 14:26:39 +0700

    collected_images no longer hardcode

--- slideshow/image_utils.py
+++ slideshow/image_utils.py
@@ -6,12 +6,8 @@ THUMBNAIL_PATH = TWITTER_IMAGE_PATH + 'thumbnails/'
 THUMBNAIL_SIZE = 60, 38
 
 def collected_images():
-    images = ['132883093_49660b8f2a_z.jpg',
-              '399107666.jpg',
-              '2957442267_6afce8dd8f_z.jpg',
-              '5975249429_4c076b25c1_z.jpg',
-              '6041022237_287ede031f_z.jpg',]
-    return images
+    dir_names, file_names = default_storage.listdir(TWITTER_IMAGE_PATH)
+    return file_names 
 
 def create_thumbnail(image_name):
     image_path = default_storage.path(TWITTER_IMAGE_PATH + image_name)
