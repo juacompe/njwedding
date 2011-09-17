69fb066e306e0d961edf2f1acac1ada6f4b46223
chanita <aireanalistic@gmail.com>
2011-09-17 21:37:33 +0700

    Now  do  get_image_url_from_raw_html(raw_html, TWITPIC) instead

--- feed/regex.py
+++ feed/regex.py
@@ -1,9 +1,9 @@
-from feed.tests.regex_test_content import raw_content
 import re
 
-regex_text='id="photo-display" src="(?P<src>.+?)"'
+#Twitpic regex example:
+#regex_text='id="photo-display" src="(?P<src>.+?)"'
 
-def get_image_url_from_raw_html( content ):
+def get_image_url_from_raw_html( content, regex_text ):
     regex = re.compile( regex_text )
     r = regex.search( content )
     if not r:
