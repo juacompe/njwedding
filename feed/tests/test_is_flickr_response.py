d301e660daf5fb79ce11a31564d589172e07fa4d
Natty <natty@pollock.local>
2011-09-18 23:14:50 +0700

    change test name

--- feed/tests/test_is_flickr_response.py
+++ feed/tests/test_is_flickr_response.py
@@ -3,7 +3,7 @@ from feed.fetch_twitter import is_flickr_response
 from mock import Mock
 
 class TestIsLockerzResponse(TestCase):
-    def test_is_flickr_response_is_true_for_instagram(self):
+    def test_is_flickr_response_is_true_for_flickr(self):
         location = 'http://www.flickr.com/photos/63701352@N06/6158805817/'
         response = Mock()
         response.get = Mock(return_value = location)
