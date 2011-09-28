2f13e1c44672d576936cabc78f05509840a13684
juacompe <juacompe@lufia.(none)>
2011-09-28 11:00:52 +0700

    - feed/tests/test_models.py still fails, but Natty found a way to parse date string

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db.models import Model, TextField, DateTimeField, CharField
 
 # Create your models here.
+class Tweet(Model):
+    text = TextField()
+    created_at = DateTimeField()
+    id_str = CharField(max_length = 20)
+    profile_image_url = CharField(max_length = 200)
+    from_user = CharField(max_length = 50)
+
+
