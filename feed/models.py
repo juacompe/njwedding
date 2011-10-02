0698944b08a4aeb67f2ed83d96fa0e4acca723e1
juacompe <juacompe@lufia.(none)>
2011-10-02 13:21:15 +0700

    polishing model

--- feed/models.py
+++ feed/models.py
@@ -7,3 +7,8 @@ class Tweet(Model):
     profile_image_url = CharField(max_length = 200)
     from_user = CharField(max_length = 50)
 
+    def __unicode__(self):
+        return '%s: %s' % (self.from_user, self.text)
+
+    class Meta:
+        ordering = ['created_at']
