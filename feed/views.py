38c01200b70864a7c3fdc4759f37293e1e3b97e6
juacompe <juacompe@lufia.(none)>
2011-10-02 22:53:17 +0700

    - redump the fixture - feed view's tests pass

--- feed/views.py
+++ feed/views.py
@@ -5,9 +5,10 @@ from feed.models import Tweet
 from json import dumps
 
 def new_tweets(request):
+    tweets = Tweet.objects.all().extra(order_by = ['-created_at'])
     json_str = dumps({'max_id_str': '117972896940437504',
                       'next_page': '/api/new_tweets/after/117972896940437504',
-                      'results': [ model_to_dict(tweet) for tweet in Tweet.objects.all() ]
+                      'results': [ model_to_dict(tweet) for tweet in tweets ]
                      }, cls = DjangoJSONEncoder)
     return HttpResponse(json_str)
 
